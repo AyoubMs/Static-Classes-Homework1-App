1fc3dfb0e5dda9573c06ef874a3522d6511500da
AyoubMS <chahayb563@gmail.com>
2023-07-05 14:33:34 +0200

    add app

---
+++ StaticClassesHomework1/Calculations.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassesHomework1
+{
+    public static class Calculations
+    {
+        public static double CalculateDoubles(double a, double b, char operation)
+        {
+            switch (operation)
+            {
+                case '*':
+                    return a * b;
+                case '/':
+                    return a / b;
+                case '-':
+                    return a - b;
+                case '+':
+                    return a + b;
+                default:
+                    break;
+            }
+            return 0.0;
+        }
+
+        public static int CalculateInts(int a, int b, char operation)
+        {
+            switch (operation)
+            {
+                case '*':
+                    return a * b;
+                case '/':
+                    return a / b;
+                case '-':
+                    return a - b;
+                case '+':
+                    return a + b;
+                default:
+                    break;
+            }
+            return 0;
+        }
+
+        public static decimal CalculateDecimals(decimal a, decimal b, char operation)
+        {
+            switch (operation)
+            {
+                case '*':
+                    return a * b;
+                case '/':
+                    return a / b;
+                case '-':
+                    return a - b;
+                case '+':
+                    return a + b;
+                default:
+                    break;
+            }
+            return 0m;
+        }
+    }
+}
